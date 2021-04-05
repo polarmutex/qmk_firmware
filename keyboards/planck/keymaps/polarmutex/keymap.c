3f69b65da413fba0274589ac4ecfcba6383d727e
Brian Ryall <brianryall@fastmail.com>
2021-04-04 22:59:16 -0400

    add ctrl|shift to x key

--- keyboards/planck/keymaps/polarmutex/keymap.c
+++ keyboards/planck/keymaps/polarmutex/keymap.c
@@ -47,10 +47,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_planck_grid(
-    KC_TAB,         KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-    LCTL_T(KC_ESC), KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    RCTL_T(KC_ENT),
-    KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-    BACKLIT,        KC_LCTL, KC_LALT, KC_LGUI, LOWER,   KC_SPC,  S_SFT,   RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
+    KC_TAB,         KC_Q,    KC_W,        KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+    LCTL_T(KC_ESC), KC_A,    KC_R,        KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    RCTL_T(KC_ENT),
+    KC_LSFT,        KC_Z,    RCS_T(KC_X), KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    BACKLIT,        KC_LCTL, KC_LALT,     KC_LGUI, LOWER,   KC_SPC,  S_SFT,   RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Colemak DH Mod Shifted
