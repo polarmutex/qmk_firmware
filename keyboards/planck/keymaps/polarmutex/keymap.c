e7dee09bf93b8217fa11f10c4b3f20d55b0501fd
Brian Ryall <me@brianryall.xyz>
2024-10-13 17:03:29 -0400

    fix: update to default colemak

--- keyboards/planck/keymaps/polarmutex/keymap.c
+++ keyboards/planck/keymaps/polarmutex/keymap.c
@@ -37,37 +37,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Colemak DH Mod
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   q  |   w  |   f  |   p  |   b  |   j  |   l  |   u  |   y  |   ;  | Bksp |
+ * | Tab  |   q  |   w  |   f  |   p  |   g  |   j  |   l  |   u  |   y  |   ;  | Bksp |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  |   a  |   r  |   s  |   t  |   g  |   m  |   n  |   e  |   i  |   o  |Enter |
+ * | Esc  |   a  |   r  |   s  |   t  |   d  |   h  |   n  |   e  |   i  |   o  |Enter |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Shift|   z  |   x  |   c  |   d  |   v  |   k  |   h  |   ,  |   .  |   /  |Shift |
+ * | Shift|   z  |   x  |   c  |   v  |   b  |   k  |   m  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Brite| Ctrl | Alt  | GUI  |Lower |Space |CShift|Raise | Left | Down |  Up  |Right |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = LAYOUT_planck_grid(
-    KC_TAB,         KC_Q,    KC_W,        KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
-    LCTL_T(KC_ESC), KC_A,    KC_R,        KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    RCTL_T(KC_ENT),
-    KC_LSFT,        KC_Z,    RCS_T(KC_X), KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+    KC_TAB,         KC_Q,    KC_W,        KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC,
+    LCTL_T(KC_ESC), KC_A,    KC_R,        KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    RCTL_T(KC_ENT),
+    KC_LSFT,        KC_Z,    RCS_T(KC_X), KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
     BACKLIT,        KC_LCTL, KC_LALT,     KC_LGUI, LOWER,   KC_SPC,  S_SFT,   RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT
 ),
 
 /* Colemak DH Mod Shifted
  * ,-----------------------------------------------------------------------------------.
- * | Tab  |   Q  |   W  |   F  |   P  |   B  |   J  |   L  |   U  |   Y  |   ;  |      |
+ * | Tab  |   Q  |   W  |   F  |   P  |   G  |   J  |   L  |   U  |   Y  |   ;  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Esc  |   A  |   R  |   S  |   T  |   G  |   M  |   N  |   E  |   I  |   O  |      |
+ * | Esc  |   A  |   R  |   S  |   T  |   D  |   H  |   N  |   E  |   I  |   O  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |   Z  |   X  |   C  |   D  |   V  |   K  |   H  |   ,  |   .  |   /  |      |
+ * |      |   Z  |   X  |   C  |   V  |   B  |   K  |   M  |   ,  |   .  |   /  |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_SHIFT_COLEMAK] = LAYOUT_planck_grid(
-    S(KC_TAB), S(KC_Q), S(KC_W), S(KC_F), S(KC_P), S(KC_B), S(KC_J), S(KC_L), S(KC_U),    S(KC_Y),    S(KC_SCLN), _______,
-    S(KC_ESC), S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_G), S(KC_M), S(KC_N), S(KC_E),    S(KC_I),    S(KC_O),    _______,
-    _______,   S(KC_Z), S(KC_X), S(KC_C), S(KC_D), S(KC_V), S(KC_K), S(KC_H), S(KC_COMM), S(KC_DOT),  S(KC_SLSH), _______,
+    S(KC_TAB), S(KC_Q), S(KC_W), S(KC_F), S(KC_P), S(KC_G), S(KC_J), S(KC_L), S(KC_U),    S(KC_Y),    S(KC_SCLN), _______,
+    S(KC_ESC), S(KC_A), S(KC_R), S(KC_S), S(KC_T), S(KC_D), S(KC_H), S(KC_N), S(KC_E),    S(KC_I),    S(KC_O),    _______,
+    _______,   S(KC_Z), S(KC_X), S(KC_C), S(KC_V), S(KC_B), S(KC_K), S(KC_M), S(KC_COMM), S(KC_DOT),  S(KC_SLSH), _______,
     _______,   _______, _______, _______, _______, _______, _______, _______, S(KC_LEFT), S(KC_DOWN), S(KC_UP),   S(KC_RGHT)
 ),
 
@@ -114,16 +114,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |MUSmod|Aud on|Audoff|AGnorm|AGswap|      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|TermOn|TermOf|      |      |      |
+ * |      |Voice-|Voice+|Mus on|Musoff|MIDIon|MIDIof|      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * |      |      |      |      |      |             |      |      |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_ADJUST] = LAYOUT_planck_grid(
-    _______, RESET,   DEBUG,   RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD,  RGB_VAI, RGB_VAD, _______,
-    _______, _______, MU_MOD,  AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______,  _______, _______, _______, _______,
-    _______, MUV_DE,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______
+    _______, QK_BOOT, DB_TOGG, UG_TOGG, UG_NEXT, UG_HUEU, UG_HUED, UG_SATU, UG_SATD, UG_VALU, UG_VALD, KC_DEL,
+    _______, _______, MU_NEXT, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, _______, _______, _______, _______, _______,
+    _______, AU_PREV, AU_NEXT, MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  _______, _______, _______, _______, _______,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 )
 
 };
